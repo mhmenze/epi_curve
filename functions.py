41ce404561c13779e0c43d255ade92d76e5d006b
Muhammad Nazeer <mhmenze@gmail.com>
2021-10-30 13:33:53 +0530

    epi commit 1

---
+++ functions.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+data = pd.read_csv('owid-covid-data.csv')
+
+cols = data.columns
+x_var = st.sidebar.selectbox('Pick first stat', cols, index=5)
+y_var = st.sidebar.selectbox('Pick second stat', cols, index=8)
+
+countries = set(data['location'])
+country = st.sidebar.selectbox('Pick your country', countries)
+
+
+def filter_data(df, col_names, country_name):
+
+    df = df[df['location'] == country_name]
+    col_names = ['date'] + col_names
+    df = df[col_names]
+    df['date'] = pd.to_datetime(df['date'])
+
+    return df
+
+
+data_f = filter_data(data, [x_var] + [y_var], country)
+
+source = data_f
+base = alt.Chart(source).encode(
+    alt.X('date', axis=alt.Axis(title=None))
+)
+
+line1 = base.mark_line(stroke='#57A44C', interpolate='monotone').encode(
+    alt.Y(x_var,
+          axis=alt.Axis(title=x_var, titleColor='#57A44C'))
+)
+
+line = base.mark_line(stroke='#5276A7', interpolate='monotone').encode(
+    alt.Y(y_var,
+          axis=alt.Axis(title=y_var, titleColor='#5276A7'))
+)
+
+c = alt.layer(line1, line).resolve_scale(
+    y='independent'
+)
+
+st.altair_chart(c, use_container_width=True)
